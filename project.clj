b0abe368e2daf2f89aad65e921dc3cdb9a94d9df
Tom Jakubowski <tom@crystae.net>
2013-11-23 02:44:10 -0800

    add hidapi dep, print blink1 devices to stdout

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [local/blink1 "0.0.1-SNAPSHOT"]]
+                 [org.clojars.torbjornvatn/hidapi "1.1"]]
   :main ^:skip-aot blink1.core
   :target-path "target/%s"
   :repositories {"project" "file:repo"}
--- src/blink1/core.clj
+++ src/blink1/core.clj
@@ -1,8 +1,28 @@
 (ns blink1.core
-  (:import thingm.blink1.Blink1)
+  (:import [com.codeminders.hidapi HIDDeviceInfo HIDManager]
+           [java.io IOException])
   (:gen-class))
+
+(defn load-hid-natives []
+  (let [bits (System/getProperty "sun.arch.data.model")]
+    (clojure.lang.RT/loadLibrary (str "hidapi-jni-" bits))))
+
+(def blink1-vid 10168)
+(def blink1-pid 493)
+
+(defn blink1? [dev]
+  (and
+    (= blink1-vid (.getVendor_id dev))
+    (= blink1-pid (.getProduct_id dev))))
+
+(defn blink1-devices []
+  (let [manager (HIDManager/getInstance)]
+    (filter blink1? (.listDevices manager))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (println "Loading HID natives...")
+  (load-hid-natives)
+  (print (blink1-devices))
   (println "Hello, World!"))
