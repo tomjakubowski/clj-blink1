cc8a67743d81f6c4e9f3b17257e948d3366484c4
Tom Jakubowski <tom@crystae.net>
2013-11-24 17:50:02 -0800

    good enough implementation of set-rgb

--- src/blink1/core.clj
+++ src/blink1/core.clj
@@ -1,28 +1,59 @@
 (ns blink1.core
-  (:import [com.codeminders.hidapi HIDDeviceInfo HIDManager]
-           [java.io IOException])
+  (:import [com.codeminders.hidapi HIDDeviceInfo HIDManager HIDDeviceNotFoundException]
+           [java.io IOException]
+           [java.nio ByteBuffer])
   (:gen-class))
 
-(defn load-hid-natives []
+(defn- load-hid-natives []
   (let [bits (System/getProperty "sun.arch.data.model")]
     (clojure.lang.RT/loadLibrary (str "hidapi-jni-" bits))))
 
-(def blink1-vid 10168)
-(def blink1-pid 493)
+(def ^{:private true} blink1-vid 10168)
+(def ^{:private true} blink1-pid 493)
 
-(defn blink1? [dev]
-  (and
-    (= blink1-vid (.getVendor_id dev))
-    (= blink1-pid (.getProduct_id dev))))
+(defn open-blink1 []
+  (.openById (HIDManager/getInstance) blink1-vid blink1-pid nil))
 
-(defn blink1-devices []
-  (let [manager (HIDManager/getInstance)]
-    (filter blink1? (.listDevices manager))))
+(def ^{:private true} blink1-report-id 1)
+(def ^{:private true} blink1-buf-size 9)
+(def ^{:private true} blink1-cmd-set-rgb (first (.getBytes "n")))
+
+(defn set-rgb [dev r g b]
+  (let [buflen blink1-buf-size
+        bb (ByteBuffer/allocate buflen)
+        buf (byte-array buflen)]
+    (doto bb
+      (.put (.byteValue blink1-report-id))
+      (.put (.byteValue blink1-cmd-set-rgb))
+      (.put (.byteValue r))
+      (.put (.byteValue g))
+      (.put (.byteValue b))
+      (.rewind)
+      (.get buf))
+    (println (map #(bit-and % 255) (into [] buf)))
+    (.write dev buf)
+    dev))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Loading HID natives...")
   (load-hid-natives)
-  (print (blink1-devices))
+  (println "opening blink(1)...")
+  (try 
+    (if-let [blink1 (open-blink1)]
+      (try
+        (println (str "blink1: (" blink1 ")"))
+        (set-rgb blink1 0 255 0)
+        (finally
+          (.close blink1)
+          (.release (HIDManager/getInstance)))))
+    (catch HIDDeviceNotFoundException e
+      (.release (HIDManager/getInstance))
+      (throw (RuntimeException. "No blink(1) device found.")))
+    (finally
+      (.release (HIDManager/getInstance))))
   (println "Hello, World!"))
+
+(comment
+  (-main))
